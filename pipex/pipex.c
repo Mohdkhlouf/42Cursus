52d6ac4f5a3b3db990a151243b3d78f18460f368
Mohammad <mohammad.khlouf@gmail.com>
2025-01-29 13:46:54 +0200

    uploading

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mkhlouf <mkhlouf@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:16:45 by mkhlouf           #+#    #+#             */
-/*   Updated: 2025/01/27 10:41:42 by mkhlouf          ###   ########.fr       */
+/*   Updated: 2025/01/29 00:25:04 by mkhlouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,48 @@ void Exit_print_Error()
     exit(-1);
 }
 //check commands functions
-void check_files(char *cmd1, char *cmd2, t_pipex *pipex)
+// void free_2d_arr(char **arr)
+// {
+//     while (*arr)
+//     {
+//         free(*arr);
+//         arr++;
+//     }
+//     free(arr);
+// }
+
+void check_commands_exisit_mode(char *filename, int mode)
 {
-    pipex->cmd1 = cmd1;
-    pipex->cmd2 = cmd2;
+    if (access(filename, F_OK|mode) == 0)
+        return;
+    else
+        Exit_print_Error();
+}
+
+
+void check_commands(char *cmd1, char *cmd2, t_pipex *pipex)
+{
+    char **path_cmd1_arr;
+    char **path_cmd2_arr;
+    char *path_cmd1;
+    char *path_cmd2;
+
+    path_cmd1_arr = ft_split(cmd1, ' ');
+    path_cmd2_arr = ft_split(cmd2, ' ');
+    
+    path_cmd1 = ft_strjoin("/bin/", path_cmd1_arr[0]);
+    path_cmd2 = ft_strjoin("/bin/", path_cmd2_arr[0]);
+    check_commands_exisit_mode(path_cmd1, X_OK);
+    check_commands_exisit_mode(path_cmd2, X_OK);
+    pipex->cmd1 = ft_strdup(path_cmd1);
+    pipex->cmd2 = ft_strdup(path_cmd2);
+    // free_2d_arr(path_cmd1_arr);
+    // free_2d_arr(path_cmd2_arr);
+    // free(path_cmd1);
+    // free(path_cmd2);
+   
+    ft_printf("cmd1 path:#%s#\ncmd2 path:#%s#\n", pipex->cmd1 , pipex->cmd2);
+
 }
 //--end of check commands functions
 
@@ -62,8 +100,12 @@ void check_arguments(int argc, char **argv, t_pipex *pipex)
 int main(int argc, char **argv)
 {
     t_pipex *pipex;
-    pipex = malloc(sizeof pipex);
+    int pipefd[2];
 
+    pipex = malloc(sizeof pipex);
     check_arguments(argc, argv, pipex);
+
+    pipe(pipefd);
+    
     return (0);
 }
