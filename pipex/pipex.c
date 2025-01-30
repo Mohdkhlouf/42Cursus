36ca149fa76219348ba71de1c0a9c349f80a1c6a
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-01-30 16:40:22 +0200

    uploading

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mkhlouf <mkhlouf@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 17:16:45 by mkhlouf           #+#    #+#             */
-/*   Updated: 2025/01/29 15:27:27 by mkhlouf          ###   ########.fr       */
+/*   Updated: 2025/01/30 16:31:01 by mkhlouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,28 +36,69 @@ void check_commands_exisit_mode(char *filename, int mode)
         Exit_print_Error();
 }
 
+char **parse_path(char *env[])
+{
+    int i;
+    char **path;
+    i = 0;
+    while (env[i])
+    {
+        if (ft_strnstr(env[i], "PATH=", 5))
+        {
+            printf("%s", env[i]);
+            path = ft_split(ft_strdup(env[i]),":");
+            if(!path)
+                Exit_print_Error();
+            return (path);
+        } 
+        i++;
+    }
+    return (NULL);
+}
 
-void check_commands(char *cmd1, char *cmd2, t_pipex *pipex)
+void compare_commands(char *path, char * cmd1, char * cmd2)
+{
+    int i;
+    
+    i = 0;
+    while ( path[i])
+    {
+        if (check_commands_exisit_mode());
+    }
+        
+            
+    
+    
+}
+void check_commands(char *cmd1, char *cmd2, t_pipex *pipex, char *env[])
 {
     char **path_cmd1_arr;
     char **path_cmd2_arr;
     char *path_cmd1;
     char *path_cmd2;
-
+    int i;
+    char **path;
+    
+    i = 0;
     path_cmd1_arr = ft_split(cmd1, ' ');
     path_cmd2_arr = ft_split(cmd2, ' ');
     
-    path_cmd1 = ft_strjoin("/bin/", path_cmd1_arr[0]);
-    path_cmd2 = ft_strjoin("/bin/", path_cmd2_arr[0]);
-    check_commands_exisit_mode(path_cmd1, X_OK);
-    check_commands_exisit_mode(path_cmd2, X_OK);
-    pipex->cmd1 = ft_strdup(path_cmd1);
-    pipex->cmd2 = ft_strdup(path_cmd2);
+    path = parse_path(env);
+    if (path)
+        compare_commands(path, path_cmd1_arr[0], path_cmd2_arr[0]);
+    // path_cmd1 = ft_strjoin("/bin/", path_cmd1_arr[0]);
+    // path_cmd2 = ft_strjoin("/bin/", path_cmd2_arr[0]);
+    // check_commands_exisit_mode(path_cmd1, X_OK);
+    // check_commands_exisit_mode(path_cmd2, X_OK);
+    // pipex->cmd1 = ft_strdup(path_cmd1);
+    // pipex->cmd2 = ft_strdup(path_cmd2);
+    
     // free_2d_arr(path_cmd1_arr);
     // free_2d_arr(path_cmd2_arr);
     // free(path_cmd1);
     // free(path_cmd2);
    
+  
     ft_printf("cmd1 path:#%s#\ncmd2 path:#%s#\n", pipex->cmd1 , pipex->cmd2);
 
 }
@@ -86,14 +127,14 @@ void check_files(char *infile, char *outfile, t_pipex *pipex)
 
 
 
-void check_arguments(int argc, char **argv, t_pipex *pipex)
+void check_arguments(int argc, char **argv, t_pipex *pipex, char *env[])
 {
     if(argc != 5)
             ft_putstr_fd("Error\n", 2);
     else
     {
         check_files(argv[1], argv[4], pipex);
-        check_commands(argv[2], argv[3], pipex);
+        check_commands(argv[2], argv[3], pipex, env);
     }
 }
 
@@ -103,11 +144,10 @@ int main(int argc, char **argv, char *env[] )
     int pipefd[2];
 
     pid_t pid;
-    
     pipex = malloc(sizeof pipex);
-    check_arguments(argc, argv, pipex);
+    check_arguments(argc, argv, pipex, env);
 
     pipe(pipefd);
-    
+    pid = pipe(pipefd);
     return (0);
 }
