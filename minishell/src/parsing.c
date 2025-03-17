1f6ed29f1ce64993001d988a1126595f5ffd2d83
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-03-17 13:27:43 +0200

    upload

--- minishell/src/parsing.c
+++ minishell/src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mkhlouf <mkhlouf@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 10:02:18 by mkhlouf           #+#    #+#             */
-/*   Updated: 2025/03/17 13:02:43 by mkhlouf          ###   ########.fr       */
+/*   Updated: 2025/03/17 13:27:12 by mkhlouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,10 @@ int	how_many_parts(t_data *data)
 
 void	line_split(t_data *data)
 {
-	size_t i;
-	int j;
-	int start;
-	bool skip;
+	size_t	i;
+	int		j;
+	int		start;
+	bool	skip;
 
 	i = 0;
 	j = 0;
@@ -55,31 +55,37 @@ void	line_split(t_data *data)
 		return ;
 	}
 	while (data->cleaned_line[i])
+	{
+		if (data->cleaned_line[i] == '\'' || data->cleaned_line[i] == '\"')
 		{
-			if(data->cleaned_line[i] == 27 || data->cleaned_line[i] == 22)
+			if (skip == false)
 			{
-				if (skip == false)
-					skip = true;
-				else
-					skip = false;
 				start = i;
-				if (skip == false)
-				{
-					data->tokens[j].data = ft_substr(data->cleaned_line, start, i - start +1);
-					start = i + 1;
-					printf("token #%s#\n", data->tokens[j].data);
-					j++;
-				}
+				skip = true;
 			}
-			if ((data->cleaned_line[i] == ' ' || data->cleaned_line[i + 1] == '\0') && !skip)
+			else
+				skip = false;
+			
+			if (skip == false)
 			{
-				data->tokens[j].data = ft_substr(data->cleaned_line, start, i - start);
-				start = i + 1;
+				data->tokens[j].data = ft_substr(data->cleaned_line, start, i
+						- start + 1);
 				printf("token #%s#\n", data->tokens[j].data);
+				printf("j #%d#\n", j);
 				j++;
 			}
-			
-			i++;
 		}
-		
+		if ((data->cleaned_line[i] == ' ' || data->cleaned_line[i
+				+ 1] == '\0') && !skip)
+		{
+			data->tokens[j].data = ft_substr(data->cleaned_line, start, i
+					- start);
+			start = i + 1;
+			printf("token #%s#\n", data->tokens[j].data);
+			printf("j #%d#\n", j);
+
+			j++;
+		}
+		i++;
+	}
 }
