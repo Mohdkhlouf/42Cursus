1736c34db91324423009c2accdf00a6f1930b022
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-04-02 16:00:50 +0300

    starting with parsing

--- minishell/src/parsing/parsing.c
+++ minishell/src/parsing/parsing.c
@@ -6,14 +6,38 @@
 /*   By: mkhlouf <mkhlouf@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 14:56:54 by mkhlouf           #+#    #+#             */
-/*   Updated: 2025/04/02 15:20:31 by mkhlouf          ###   ########.fr       */
+/*   Updated: 2025/04/02 16:00:19 by mkhlouf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/parsing.h"
 
-void parsing(t_data *data)
+void	cmds_init(t_parsed_data *cmds_data)
 {
-	
-	printf("test %s", data->input_line);
+	cmds_data->cmds_counter = 0;
+	cmds_data->pipes_counter = 0;
+}
+
+void	find_cmds_counter(t_data *data, t_parsed_data *cmds_data)
+{
+	int	i;
+
+	i = 0;
+	while (data->input_line[i])
+	{
+		if (data->input_line[i] == '|')
+			cmds_data->pipes_counter++;
+		i++;
+	}
+	cmds_data->cmds_counter = cmds_data->pipes_counter + 1;
+}
+
+void	parsing(t_data *data)
+{
+	t_parsed_data *cmds_data;
+
+	cmds_data = malloc(sizeof(t_parsed_data) * 1);
+	cmds_init(cmds_data);
+	find_cmds_counter(data, cmds_data);
+	printf("i have %d commands\n", cmds_data->cmds_counter);
 }
