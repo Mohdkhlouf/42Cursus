a7a7dec4f4bfa051c5768c5230954c9c060c3982
Mohammad <mohammad.khlouf@gmail.com>
2025-07-23 02:29:08 +0300

    fix ex04

--- cpp/cpp01/ex04/main.cpp
+++ cpp/cpp01/ex04/main.cpp
@@ -1,57 +1,72 @@
 
-#include <iostream>
-#include <fstream>
-#include <cstring>
 #include <cerrno>
+#include <cstring>
+#include <fstream>
+#include <iostream>
 #include <sstream>
 
-std::string replace_words(std::string &new_str, std::string readStr, std::string s1, std::string s2)
+std::string replace_words(std::string readStr, std::string s1, std::string s2)
 {
 	std::string temp;
-	(void) s1;
-	(void) s2;
-	
 	std::istringstream ss(readStr);
-	while(ss >> temp)
-		if(temp == s1)
-			new_str.append(s2).append(" ");
-		else if (temp != s1)
-			new_str.append(temp).append(" ");
-		else
-			new_str.append(temp);
-	return new_str;
+	std::string new_str;
+	std::size_t found;
+	found = readStr.find(s1);
+	while (found != std::string::npos)
+	{
+		new_str.append(readStr.substr(0, found));
+		new_str.append(s2);
+		readStr.erase(0, found+ s1.length());
+		found = readStr.find(s1);
+	}
+	new_str.append(readStr);
+	return (new_str);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cerr << "must be 4 arguments" << "\n";
+		std::cerr << "must be 4 arguments"
+					<< "\n";
 		return (1);
 	}
-	std::string fileName = argv[1];
+	std::string inFile = argv[1];
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
+	std::string outFile = inFile + ".replace";
 	std::string readStr;
-	std::ifstream file(fileName);
+	std::ifstream file(inFile);
+	std::ofstream ofile(outFile);
 	std::string new_str;
-	if (!file)
+	if (!file || !ofile)
 	{
 		std::cerr << "Error opening the file:" << std::strerror(errno) << "\n";
 		return (errno);
 	}
-	try
+	while (std::getline(file, readStr))
 	{
-		while (std::getline(file, readStr))
-			replace_words(new_str, readStr, s1, s2);
-		std::cout<<new_str<<std::endl;
+		if (readStr.find(s1) != std::string::npos)
+		{
+			new_str.append(replace_words(readStr, s1, s2));
+			new_str.append("\n");
+		}
+		else
+		{
+			new_str.append(readStr);
+			new_str.append("\n");
+		}
 	}
-	catch (std::exception &e)
+	if (file.eof())
+		if (new_str.back() == '\n')
+			new_str.pop_back();
+	if (file.bad())
 	{
-		std::cerr << "error while reading the line: " << e.what() << "\n";
-		return (1);
+		std::cerr << "Error while reading line: " << strerror(errno) << "\n";
+		return (errno);
 	}
-
+	std::cout << new_str << std::endl;
 	file.close(); // needs also try and catch if it fails;
+	ofile.close();
 	return (0);
 }
