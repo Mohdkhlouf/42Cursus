c89ebccac1f831346ace6c6d5f705b8b029a5721
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-09-19 15:36:03 +0300

    upload:

---
+++ cpp/cpp08/ex00/easyfind.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <stdexcept>
+#include <exception>
+#include <iterator>
+#include <iostream>
+#include <algorithm>
+
+class value_not_found : public std::exception{
+    private:
+        std::string message;
+
+    public:
+        value_not_found(const std::string &msg):message(msg){};
+        const char * what() const noexcept override{
+            return (message.c_str());
+        }
+};
+
+template <typename T>
+typename T::iterator easyfind(T &v, int v2){
+    typename T::iterator it = std::find(v.begin(), v.end(), v2);
+    if (it != v.end()){
+        return (it);
+	}
+	throw value_not_found("Element not found");
+}
---
+++ cpp/cpp08/ex00/main.cpp
@@ -0,0 +1,40 @@
+#include "easyfind.hpp"
+#include <vector>
+
+
+void vector_test(){
+    {
+        std::cout<<"Int Vector case 1\n";
+        std::vector<int> int_vec={1,2,3,4,5,6};
+        std::cout<<"result: "<<*easyfind(int_vec, 5)<<std::endl;
+        int_vec.push_back(10);
+        std::cout<<"result: "<<*easyfind(int_vec, 10)<<std::endl;
+    }
+
+    {
+        std::cout<<"Char Vector case 2\n";
+        std::vector<char> char_vec={'a','b','c'};
+        std::cout<<"Vector members:{";
+        for (char c:char_vec){
+            std::cout<<"'"<<c<<"'";
+        }
+        std::cout<<"}\n";
+        std::cout<<"result when (b): "<<*easyfind(char_vec, 'b')<<std::endl;
+        char_vec.push_back('d');
+        std::cout<<"result wne (d): "<<*easyfind(char_vec, 'd')<<std::endl;
+    }
+
+
+}
+
+int main(){
+    try{
+        vector_test();
+        return (0);
+    }catch (const value_not_found &e){
+        std::cout<<"Error: "<<e.what()<<std::endl;
+    }
+
+
+	return (0);
+}
