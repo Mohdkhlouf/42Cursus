37b3eccd141991cc9dd2416ab89e0a662038dc2e
mohdkhlouf <mohammad.khlouf@gmail.com>
2025-09-26 13:28:59 +0300

    test

--- cpp/cpp08/ex01/Span.hpp
+++ cpp/cpp08/ex01/Span.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
-
-
+#include <iostream>
+#include <limits>
 class Span{
 
 	private:
@@ -17,7 +17,17 @@ class Span{
 		void addNumber(long N);
 		template<typename T>
 		void addManyNumbers(T start, T end){
-			numbers.insert(numbers.begin(), start, end);
+			if( std::distance(start, end) < 0)
+				throw std::runtime_error("Error in your iterators.");
+			if (std::distance(start, end) > maxNumbers)
+				throw std::runtime_error("Overflow iterators.");
+			for(auto it = start; it != end; it++)
+			{
+				if(*it > std::numeric_limits<int>::max() || *it < std::numeric_limits<int>::min()){
+					throw std::runtime_error("Overflow iterators.");
+				}
+			}
+			numbers.insert(numbers.begin(), start, end);	
 		}
 		std::vector<int> &getNumbers();
 		void printNumbers();
--- cpp/cpp08/ex01/main.cpp
+++ cpp/cpp08/ex01/main.cpp
@@ -2,41 +2,65 @@
 #include <iostream>
 #include <random>
 
-int main(){
-
-try{
-	Span first_span(100);
-	first_span.addNumber(10);
-	first_span.addNumber(8);
-	first_span.addNumber(6);
-	first_span.addNumber(4);
-	first_span.addNumber(2);
-	first_span.printNumbers();
-	std::cout<<"Shortest Span is:"<<first_span.shortestSpan()<<std::endl;
-	std::cout<<"Longest Span is:"<<first_span.longestSpan()<<std::endl;
-}catch (std::exception &e){
-	std::cout<<"Error: "<<e.what()<<std::endl;
-}
-
-
+int main()
 {
-	Span test(1000);
-	std::vector<int> range;
-	std::vector<int> range2;
-	range2.reserve(5);
 
-	range.reserve(1000);
-	range2={1,2,3,4,5};
+	try
+	{
+		Span first_span(100);
+		first_span.addNumber(10);
+		first_span.addNumber(8);
+		first_span.addNumber(6);
+		first_span.addNumber(4);
+		first_span.addNumber(2);
+		first_span.printNumbers();
+		std::cout << "Shortest Span is:" << first_span.shortestSpan() << std::endl;
+		std::cout << "Longest Span is:" << first_span.longestSpan() << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dist(-5000, 5000);
-	for(int i = 0; i < 1000; i++)
-		range.push_back(dist(gen));
+	{
+		try
+		{
+			std::cout << "\nAdd many numbers test case" << std::endl;
+			Span test(1000);
+			std::vector<int> range;
+			range.reserve(1000);
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<long long> dist(-5000, 5465465415154);
+			for (int i = 0; i < 1000; i++)
+				range.push_back(dist(gen));
+			test.addManyNumbers(range.begin(), range.end());
+			std::cout << "Shortest Span is:" << test.shortestSpan() << std::endl;
+			std::cout << "Longest Span is:" << test.longestSpan() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
 
-	test.addManyNumbers( range.begin(), range2.end());
-	std::cout<<"Shortest Span is:"<<test.shortestSpan()<<std::endl;
-	std::cout<<"Longest Span is:"<<test.longestSpan()<<std::endl;
-}
+	{
+		try
+		{
+			std::cout << "\nSubject test case" << std::endl;
+			Span sp = Span(5);
+			sp.addNumber(6);
+			sp.addNumber(3);
+			sp.addNumber(17);
+			sp.addNumber(9);
+			sp.addNumber(11);
+			std::cout << sp.shortestSpan() << std::endl;
+			std::cout << sp.longestSpan() << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << "Error: " << e.what() << std::endl;
+		}
+	}
 	return (0);
 }
