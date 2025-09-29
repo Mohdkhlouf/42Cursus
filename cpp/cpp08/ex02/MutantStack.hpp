06ba749d18b193e24a6adbfa306d60c80d09aac5
Mohammad <mohammad.khlouf@gmail.com>
2025-09-29 21:40:57 +0300

    uplodaing

--- cpp/cpp08/ex02/MutantStack.hpp
+++ cpp/cpp08/ex02/MutantStack.hpp
@@ -1,23 +1,79 @@
 #pragma once
-
+#include <deque>
 #include <stack>
 
-template <typename T>
-class MutantStack : public std::stack<T>{
-	public:
-		class iterator{
-			private:
-				typename std::deque<T>::reverse_iterator iter_;
-			public:
+template <typename T> class MutantStack : public std::stack<T>
+{
+  public:
+	class iterator
+	{
+		private:
+		typename std::deque<T>::reverse_iterator iter_;
 
-
+		public:
+		iterator(typename std::deque<T>::reverse_iterator it) : iter_(it){};
+		T &operator*() const
+		{
+			return (*iter_);
 		};
 
+		T *operator->() const
+		{
+			return (&(*iter_));
+		};
 
+		iterator &operator++()
+		{
+			++iter_;
+			return (*this);
+		};
 
+		iterator operator++(int)
+		{
+			iterator temp = *this;
+			++iter_;
+			return (temp);
+		};
 
-		iterator begin(){
-			return (this->c.begin());
-		}
+		iterator &operator--()
+		{
+			--iter_;
+			return (*this);
+		};
 
+		iterator operator--(int)
+		{
+			iterator temp = *this;
+			--iter_;
+			return (temp);
+		};
+
+		bool operator==(const iterator &obj)const{
+			return (iter_ == obj.iter_);
+		};
+
+		bool operator!=(const iterator &obj) const{
+			return (iter_ != obj.iter_);
+		};
+	};
+
+	iterator begin()
+	{
+		return (iterator(this->c.rbegin()));
+	};
+
+	iterator end()
+	{
+		return (iterator(this->c.rend()));
+	};
+
+	iterator rbegin()
+	{
+		return (iterator(this->c.begin()));
+	};
+
+	iterator rend()
+	{
+		return (iterator(this->c.end()));
+	};
 };
--- cpp/cpp08/ex02/main.cpp
+++ cpp/cpp08/ex02/main.cpp
@@ -1,11 +1,37 @@
 #include "MutantStack.hpp"
+#include <iostream>
 
+int	main(void)
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
 
-int main(){
-	MutantStack<int> ms;
-	ms.push(10);
-	ms.push(20);
+	std::cout <<"Mutant stack top:"<<mstack.top() << std::endl;
+	mstack.pop();
+	
+	std::cout <<"\nMutant stack size:"<< mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator itEnd = mstack.end();
+	++it;
+	--it;
+	std::cout<<"\nMutant stack members:"<<std::endl;
+	while (it != itEnd)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
 
-
+	std::cout<<"\nMutant stack members with for loop:"<<std::endl;
+	for (auto iterator = mstack.begin(); iterator != mstack.end(); iterator++)
+	{
+		std::cout<<*iterator<<std::endl;
+	}
+	
 	return (0);
 }
