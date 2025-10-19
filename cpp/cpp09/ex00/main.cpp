2c2d924db31b8d84ef9010e5f182803d03177ffe
Mohammad <mohammad.khlouf@gmail.com>
2025-10-19 19:13:12 +0300

    upload

--- cpp/cpp09/ex00/main.cpp
+++ cpp/cpp09/ex00/main.cpp
@@ -1,18 +1,43 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
+#include <fstream>
+#include <cerrno>
+#include <cstring> 
 
-bool checkFile(const std::string &fileName){
-	std::cout<<"File Name is:"<<fileName<<std::endl;
-	return (true);
+void checkFile(std::ifstream &sfile,const std::string &fileName){
+	
+	sfile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+	try{
+		sfile.open(fileName);
+		std::cout<<"File Name is:"<<fileName<<std::endl;
+	}catch (const std::exception &e){
+		throw;
+	}
 }
 
 int main(int argc, char **argv){
+	std::ifstream inputFile;
+	std::ifstream datafile;
 
 	if ( argc != 2){
-		std::cout<<"Error\nit must be [btc filename]"<<std::endl;
+		std::cerr<<"Error\nit must be [btc filename]"<<std::endl;
 		return (1);
 	}
-		
-	checkFile(argv[1]);
+	try{
+		// check data file
+		checkFile(datafile,"data.csv");
+		//check input file
+		checkFile(inputFile,argv[1]);
+
+	}catch (const std::exception &e){
+		int errorCode = errno;
+		std::string systemError = std::strerror(errorCode);
+		std::cerr<<"Error\n"<<systemError<<std::endl;
+		if(datafile.is_open())
+			datafile.close();
+		if (inputFile.is_open())
+			inputFile.close();
+	}
+	
 	return (0);
 }
