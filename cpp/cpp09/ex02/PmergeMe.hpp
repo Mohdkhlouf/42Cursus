0ba28b52a1fef3d7ee812112835610ce45cd1495
Mohammad <mohammad.khlouf@gmail.com>
2025-10-25 10:49:25 +0300

    trying sorting

--- cpp/cpp09/ex02/PmergeMe.hpp
+++ cpp/cpp09/ex02/PmergeMe.hpp
@@ -27,18 +27,30 @@ class PmergeMe{
         void sortData(T &container, size_t level){
             if (container.size() < 2)
                 return;
-            bool is_odd = container.size()%2;
-            (void) level;
-            typename T::iterator itb = container.begin();
-            typename T::iterator ite = (is_odd) ? std::prev(container.end()) : container.end();
+            bool is_odd = (container.size() / level )%2;
+            // typename T::iterator itb = container.begin();
+            // typename T::iterator ite = (is_odd) ? std::prev(container.end()) : container.end();
 
-            for(auto it =itb;it != ite; it++){
-                if (*it > *(it+1)){
-                    auto temp = *it;
-                    *it = *(it+1);
-                    *(it+1) = temp;
+            // for(auto it =itb + level;it != ite; it+=(level *2)){
+            //     auto first = (it);
+            //     auto second = (it+level*2);
+            //     if (*first > *second){
+            //         auto temp = *first;
+            //         *first = *second;
+            //         *second= temp;
+            //     }
+
+            size_t end = (is_odd) ? container.size() - 1 : container.size();
+            for (size_t i = level - 1 ; i < end; i+=level*2){
+                std::cout<<"debug:"<<container[i]<<" Debug2:"<<container[i + (level*2)-1]<<std::endl;
+                if (container[i] > container[i + (level*2)-1]){
+                    auto temp = container[i];
+                    container[i] = container[i + (level*2)-1];
+                    container[i + (level*2)-1] = temp;
                 }
-                it++;
+                std::cout<<"debug:"<<i<<std::endl;
+                // std::cout<<"DEbig:"<<i+(level * 2)<<std::endl;
+
             }
             printDate(container);
         }
